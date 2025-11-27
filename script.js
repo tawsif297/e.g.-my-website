a4bd6bbd93aa8f54c48cd7e9eaa827860f123c6f
tawsif297 <tawsif928@gmail.com>
2025-11-27 20:22:38 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-var index = 0
+let index = 0;
+carousel();
+
 function carousel() {
-
-    var i;
-    var x = document.getElementsByClassName("myslides");
-
-    for (i = 0; i < x.length; i++) {
-
-        x[i].style.display = "none";
-
-    }
-
-
-index=index+1  //1
-
-if(index>x.length){    //x.length = 3
-    index = 1
+  const slides = document.getElementsByClassName("myslides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  index++;
+  if (index > slides.length) {
+    index = 1;
+  }
+  slides[index - 1].style.display = "block";
+  setTimeout(carousel, 3000); // Change slide every 3 sec
 }
-
-
- x[index - 1].style.display = "block";
-
-  setTimeout(carousel, 2000);
-
-
-
-
-}
-carousel()
-
